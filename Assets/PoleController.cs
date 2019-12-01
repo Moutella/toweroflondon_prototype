87ed926fa5aa061bbcdd1fa20248ef490c4d99ff
Moutella <joaogabrielmsc@gmail.com>
2019-12-01 14:57:23 -0300

    hmm

---
+++ Assets/PoleController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoleController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionX;
+        SphereController sphereController = other.gameObject.GetComponent<SphereController>();
+        if (!sphereController.isHolding) { 
+            sphereController.onPole = true;
+            rb.useGravity = true;
+        }
+        other.gameObject.transform.position = new Vector3(gameObject.transform.position.x, other.gameObject.transform.position.y, gameObject.transform.position.z);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.None;
+        SphereController sphereController = other.gameObject.GetComponent<SphereController>();
+        sphereController.onPole = false;
+    }
+}
---
+++ Assets/SphereController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour
+{
+    public bool onPole;
+    public bool isHolding;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+    private void OnMouseDown()
+    {
+        isHolding = true;
+        rb.useGravity = false;
+    }
+    private void OnMouseUp()
+    {
+        isHolding = false;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isHolding) { 
+            var screenPoint = Input.mousePosition;
+            screenPoint.z = 32f; //distance of the plane from the camera
+            gameObject.transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
+        }
+
+    }
+}
